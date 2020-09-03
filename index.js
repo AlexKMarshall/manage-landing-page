736a7d156493740aab1da58eff917fffae28dc43
Alex Marshall <alex.k.marshall83@gmail.com>
2020-09-03 10:17:22 +0100

    Add mobile navigation popup

--- index.js
+++ index.js
@@ -1,3 +1,39 @@
+// Mobile hamburger menu handling
+
+const menuToggle = document.getElementById("menu-toggle");
+const menu = document.getElementById("menu");
+const mobileNavOverlay = document.getElementById("mobilenav-overlay");
+
+let isMenuOpen = false;
+
+menuToggle.addEventListener("click", () => {
+  if (!isMenuOpen) {
+    openMobileNav();
+  } else {
+    closeMobileNav();
+  }
+});
+
+mobileNavOverlay.addEventListener("click", () => {
+  closeMobileNav();
+});
+
+function openMobileNav() {
+  isMenuOpen = true;
+  menuToggle.setAttribute("aria-expanded", isMenuOpen);
+  menu.classList.add("header--navigation-active");
+  mobileNavOverlay.classList.add("mobilenav--overlay-active");
+}
+
+function closeMobileNav() {
+  isMenuOpen = false;
+  menuToggle.setAttribute("aria-expanded", isMenuOpen);
+  menu.classList.remove("header--navigation-active");
+  mobileNavOverlay.classList.remove("mobilenav--overlay-active");
+}
+
+// Email subscription form handling
+
 const subscriptionForm = document.getElementById("subscription-form");
 const emailInput = document.getElementById("email-input");
 const subscriptionMessage = document.getElementById("subscription-message");
@@ -9,6 +45,7 @@ subscriptionForm.addEventListener("submit", (event) => {
     emailInput.classList.remove("subscribe--input-error");
     subscriptionMessage.classList.remove("subscribe--message-error");
     subscriptionMessage.innerText = "Thank you for subscribing";
+    // Perform api call to subscribe
   } else {
     emailInput.classList.add("subscribe--input-error");
     subscriptionMessage.classList.add("subscribe--message-error");
